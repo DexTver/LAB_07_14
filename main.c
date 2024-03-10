aa6dc5aa191b3a783e7e5a02c1d9011b2b403950
User <ivan.sharapov.05@yandex.ru>
2024-03-10 21:57:19 +0300

    Made output

--- main.c
+++ main.c
@@ -2,6 +2,8 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define NAME_LEN 20
+
 struct Athlete {
     char *name;         /* имя */
     char *university;   /* университет */
@@ -37,7 +39,7 @@ int main() {
     FILE *f;
     int n, ch;
     Athlete **users = NULL;
-    char *str;
+    char str[128];
 
     printf("Please enter the file name:\n");
     scanf("%s", filename);
@@ -62,9 +64,12 @@ int main() {
         fclose(f);
     }
 
+    printf("The file has successfully been processed!\n"
+           "To display the data, enter the command \"!print\"\n"
+           "");
+
     do {
-        fgets(str, 1024 * sizeof(char), stdin);
-        str[strlen(str) - 1] = '\0';
+        scanf("%s", str);
         if (strcasecmp(str, "!print") == 0) {
             pprint(users, n);
         } else if (strcasecmp(str, "!end") == 0) {
@@ -75,7 +80,6 @@ int main() {
                    "2 = university\n"
                    "Enter only one number!\n");
             scanf("%i", &ch);
-            getchar();
             if (ch < 1 || 2 < ch) {
                 printf("Invalid command!");
             } else {
@@ -89,7 +93,6 @@ int main() {
                    "4 = index\n"
                    "Enter only one number!\n");
             scanf("%i", &ch);
-            getchar();
             if (ch < 1 || 4 < ch) {
                 printf("Invalid command!");
             } else {
@@ -135,7 +138,7 @@ float from_str_to_float(char *x) {
 void from_str_to_int_mas(char *x, int *mas) {
     int ind = 0, j = 0;
     while (x[j] != '\0') {
-        if (x[j] == ',') {
+        if (x[j] == ';') {
             x[j] = '\0';
             mas[ind++] = from_str_to_int(x);
             x += j + 1;
@@ -156,7 +159,7 @@ Athlete *fill_struct(char *str) {
     user = (Athlete *) malloc(sizeof(Athlete));
     if (user != NULL) {
         for (tt = 0; str[tt] != '\n' && str[tt] != '\0'; ++tt) {
-            if (str[tt] == ';') {
+            if (str[tt] == ';' && ind < 5) {
                 str[tt] = '\0';
                 pole[ind++] = word;
                 word = str + tt + 1;
@@ -205,7 +208,56 @@ void add_user(Athlete **users, int n) {
     char text[1024];
     users = (Athlete **) realloc(users, (n) * sizeof(Athlete *));
     if (users != NULL) {
-        fgets(text, sizeof(text), stdin);
+        scanf("%s", text);
         users[n - 1] = fill_struct(text);
     }
+}
+
+void print_line() {
+    printf("+");
+    for (int i = 0; i < NAME_LEN + 2; printf("-"), ++i);  /* name */
+    printf("+");
+    for (int i = 0; i < 12; printf("-"), ++i);  /* university */
+    printf("+");
+    for (int i = 0; i < 5; printf("-"), ++i);   /* age */
+    printf("+");
+    for (int i = 0; i < 8; printf("-"), ++i);   /* weight */
+    printf("+");
+    for (int i = 0; i < 8; printf("-"), ++i);   /* height */
+    printf("+");
+    for (int i = 0; i < 6; printf("-"), ++i);   /* res1 */
+    printf("+");
+    for (int i = 0; i < 6; printf("-"), ++i);   /* res2 */
+    printf("+");
+    for (int i = 0; i < 6; printf("-"), ++i);   /* res3 */
+    printf("+");
+    for (int i = 0; i < 7; printf("-"), ++i);   /* index */
+    printf("+");
+    printf("\n");
+}
+
+void pprint(Athlete **users, int n) {
+    print_line();
+    printf("| Name                 | University | Age | Weight | Height | Res1 | Res2 | Res3 | Index |\n");
+    print_line();
+    for (int i = 0; i < n; ++i) {
+        printf("| %s ", users[i]->name);
+        for (int j = 0; j < NAME_LEN - strlen(users[i]->name); printf(" "), j++);
+        printf("| %s ", users[i]->university);
+        for (int j = 0; j < 10 - strlen(users[i]->university); printf(" "), j++);
+        printf("| %i  | %0.1f  ", users[i]->age, users[i]->weight);
+        if (users[i]->weight < 100) printf(" ");
+        printf("| %i    ", users[i]->height);
+        printf("| %i  ", users[i]->result[0]);
+        if (users[i]->result[0] < 100) printf(" ");
+        if (users[i]->result[0] < 10) printf(" ");
+        printf("| %i  ", users[i]->result[1]);
+        if (users[i]->result[1] < 100) printf(" ");
+        if (users[i]->result[1] < 10) printf(" ");
+        printf("| %i  ", users[i]->result[2]);
+        if (users[i]->result[2] < 100) printf(" ");
+        if (users[i]->result[2] < 10) printf(" ");
+        printf("| %0.3f |\n", users[i]->index);
+    }
+    print_line();
 }
